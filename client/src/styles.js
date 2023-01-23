cd734a98325be814a7e2782a8ec089ad79f4844f
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2023-01-23 23:52:02 +0530

    initial commit

---
+++ client/src/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgba(119,7,155, 0.8)',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
