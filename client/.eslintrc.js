459b7ed6010425cc8b7ce0877721b0b43ef18c80
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2023-01-25 17:44:58 +0530

    eslint-plugin-react

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,84 +1,86 @@
+/* eslint-disable indent */
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'plugin:react/recommended',
-      'airbnb',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    "parser": "babel-eslint",
-    plugins: [
-      'react',
-    ],
-    rules: {
-      "import/extensions": 0,
-      "react/prop-types": 0,
-      "linebreak-style": 0,
-      "react/state-in-constructor": 0,
-      "import/prefer-default-export": 0,
-      "max-len": [
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  parser: 'babel-eslint',
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/extensions': 0,
+    'react/prop-types': 0,
+    'linebreak-style': 0,
+    'react/state-in-constructor': 0,
+    'import/prefer-default-export': 0,
+    'max-len': [
         2,
-        250
+      250,
       ],
-      "no-multiple-empty-lines": [
-        "error",
+      'no-multiple-empty-lines': [
+        'error',
         {
-          "max": 1,
-          "maxEOF": 1
-        }
+          max: 1,
+          maxEOF: 1,
+        },
       ],
-      "no-underscore-dangle": [
-        "error",
+      'no-underscore-dangle': [
+        'error',
         {
-          "allow": [
-            "_d",
-            "_dh",
-            "_h",
-            "_id",
-            "_m",
-            "_n",
-            "_t",
-            "_text"
-          ]
-        }
-      ],
-      "object-curly-newline": 0,
-      "react/jsx-filename-extension": 0,
-      "react/jsx-one-expression-per-line": 0,
-      "jsx-a11y/click-events-have-key-events": 0,
-      "jsx-a11y/alt-text": 0,
-      "jsx-a11y/no-autofocus": 0,
-      "jsx-a11y/no-static-element-interactions": 0,
-      "react/no-array-index-key": 0,
-      "jsx-a11y/anchor-is-valid": [
-        "error",
-        {
-          "components": [
-            "Link"
+          allow: [
+            '_d',
+            '_dh',
+            '_h',
+            '_id',
+            '_m',
+            '_n',
+            '_t',
+            '_text',
           ],
-          "specialLink": [
-            "to",
-            "hrefLeft",
-            "hrefRight"
+        },
+      ],
+      'object-curly-newline': 0,
+      'react/jsx-filename-extension': 0,
+      'react/jsx-one-expression-per-line': 0,
+      'jsx-a11y/click-events-have-key-events': 0,
+      'jsx-a11y/alt-text': 0,
+      'jsx-a11y/no-autofocus': 0,
+      'jsx-a11y/no-static-element-interactions': 0,
+      'react/no-array-index-key': 0,
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          // eslint-disable-next-line quote-props
+          'components': [
+            'Link',
           ],
-          "aspects": [
-            "noHref",
-            "invalidHref",
-            "preferButton"
-          ]
-        }
-      ]
-    }
+          specialLink: [
+          'to',
+          'hrefLeft',
+          'hrefRight',
+        ],
+          aspects: [
+            'noHref',
+            'invalidHref',
+            'preferButton',
+          ],
+        },
+      ],
+    },
   };
